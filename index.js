a78b36234af9e4c76e4417b4b8135b83f90a5d89
eustachekamala <eustachejared25@gmail.com>
2024-06-27 09:15:40 +0300

    assignment of array lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+destructivelyAppendCat('Ralph');
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+destructivelyPrependCat('Bob');
+
+function destructiveRemoveLastCat(){
+   return cats.pop();
+}
+destructiveRemoveLastCat();
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name){
+    return [...cats,'Broom'];
+}
+
+function prependCat(){
+    return ['Arnold',...cats];
+}
+
+function removeLastCat(){
+    [...cats,cats.pop()];
+    console.log(cats);
+}
+
+function removeFirstCat(){
+    [...cats,cats.shift()];
+    console.log(cats);
+}
+
